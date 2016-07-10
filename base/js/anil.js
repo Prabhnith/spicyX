5bece7eced7361501a610f7070b35a2669087a63
anil1596 <anil.khadwal@gmail.com>
2016-07-10 21:35:49 +0530

    updation

--- base/js/anil.js
+++ base/js/anil.js
@@ -1,9 +1,22 @@
   $(document).ready(function(){
       $(window).scroll(function() { // check if scroll event happened
         if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-          $(".navbar-fixed-top").css("background-color", "rgba(0, 0, 0, 0.8)"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+          $(".ournav").css("background-color", "rgba(0, 0, 0, 0.5)"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
         } else {
-          $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+          $(".ournav").css("background-color", "transparent"); // if not, change it back to transparent
         }
       });
+    });
+
+
+    var elem = document.querySelector('.ournav');
+    var animation = elem.animate([
+        {background-color: rgba(0, 0, 0, 0.1), offset: },
+        {background-color: rgba(0, 0, 0, 0.2), offset:},
+        {background-color: rgba(0, 0, 0, 0.3), offset:},
+        {background-color: rgba(0, 0, 0, 0.4), offset:},
+    ], {
+        // direction: 'alternate',
+        // duration: 500,
+        // iterations: Infinity
     });
