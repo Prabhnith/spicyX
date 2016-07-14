e3783948382430d206cd1462953678d9c98bbd2b
anil1596 <anil.khadwal@gmail.com>
2016-07-14 21:40:05 +0530

    dahboard update

---
+++ base/js/menuItems.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    //Disable the Remove Button
+    var rowCount = $('table >tbody:last >tr').length;
+    if(rowCount == 1) {
+        document.getElementsByClassName('btn-remove')[0].disabled = true;
+    }
+    
+    $(document).on('click', '.btn-add', function(e) {
+        e.preventDefault();
+        
+        var controlForm = $('table');
+        var currentEntry = $('table>tbody>tr:last');
+        var newEntry = $(currentEntry.clone()).appendTo(controlForm);
+        // newEntry.find('.VendorMenuItem:firstChild').val('2');  
+        // newEntry.getElementsByClassName("VendorMenuItem")[0].innerHTML="2";
+                                               //Remove the Data - as it is cloned from the above
+        var rowCount = $('table >tbody:last >tr').length;
+        document.getElementsByName("VendorMenuItem")[rowCount-1].lastChild.data=rowCount;
+        //Add the button  
+        var rowCount = $('table >tbody:last >tr').length;
+        if(rowCount > 1) {
+            var removeButtons = document.getElementsByClassName('btn-remove');
+            for(var i = 0; i < removeButtons.length; i++) {
+                removeButtons.item(i).disabled = false;
+            }
+        }
+         
+    }).on('click', '.btn-remove', function(e) {
+        $(this).parents('tr:first').remove();
+        
+        //Disable the Remove Button
+        var rowCount = $('table >tbody:last >tr').length;
+        if(rowCount == 1) {
+            document.getElementsByClassName('btn-remove')[0].disabled = true;
+        }
+
+        e.preventDefault();
+        return false;
+    });
+});
